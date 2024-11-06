eaba0b5c59ee2acc62db021642c0f9256c8c0c0e
daniel-eguiluz <87681237+daniel-eguiluz@users.noreply.github.com>
2024-11-06 09:48:29 -0300

    fix frontend

--- payroll-frontend/src/components/ListadoUsuarios.jsx
+++ payroll-frontend/src/components/ListadoUsuarios.jsx
@@ -55,7 +55,7 @@ export default function ListadoUsuarios() {
 
   return (
     <div className="container">
-      <h1>Lista de clientes</h1>
+      <h1>Listado de solicitudes(chile)</h1>
 
       {/* Botón para Agregar Usuario */}
       <div className="mb-3 d-flex justify-content-start">
